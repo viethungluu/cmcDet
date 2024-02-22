2682904cb8a308a501e3561b87e68c4c5f9d3bee
HungLV <hunglv@piv.asia>
2024-02-22 23:08:38 +0700

    fix transforms

--- dataset/colorspace_transforms.py
+++ dataset/colorspace_transforms.py
@@ -4,13 +4,13 @@ from skimage import color
 from albumentations.core.transforms_interface import ImageOnlyTransform
 
 class RGB2Lab(ImageOnlyTransform):
-    """Convert RGB PIL image to ndarray Lab."""
-    def __call__(self, img, **params):
+    """Convert ndarray image to ndarray Lab."""
+    def apply(self, img: np.ndarray, **params):
         img = color.rgb2lab(img)
         return img
 
 class RGB2YCbCr(ImageOnlyTransform):
-    """Convert RGB PIL image to ndarray YCbCr."""
-    def __call__(self, img: np.ndarray, **params):
+    """Convert ndarray image to ndarray YCbCr."""
+    def apply(self, img: np.ndarray, **params):
         img = color.rgb2ycbcr(img)
         return img
