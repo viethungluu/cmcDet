f56542b0bb7fb7bd367df2748e9424a62b6bcc5f
HungLV <hunglv@piv.asia>
2024-02-23 11:16:48 +0700

    format checkpoint name

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ def handle_train(args):
                             verbose=True,
                             monitor='map',
                             mode='max',
-                            filename='{0}-{1}-{epoch}-{map:.3f}'.format(args.backbone_choice, 'pretrained' if args.mc_weights_path else 'scratch'))
+                            filename='{0}-{1}-{epoch}-{map:.3f}'.format(args.backbone_choice, 'pretrained' if args.cmc_weights_path else 'scratch'))
         ])
 
     trainer.fit(m, dm)
