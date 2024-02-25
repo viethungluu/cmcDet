22ca91e9f00cbf17d1ae6aad84d2bc441024139c
HungLV <hunglv@piv.asia>
2024-02-26 05:03:26 +0700

    reset metric on epoch end

--- models/cmc_retinanet.py
+++ models/cmc_retinanet.py
@@ -55,6 +55,9 @@ class RetinaNetModule(L.LightningModule):
 
         preds = self.model(images, targets)
         self.metric.update(preds, targets)
+        
+    
+    def validation_epoch_end(self):
         map_dict = self.metric.compute()
         self.log('map', map_dict['map'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
         self.log('map_50', map_dict['map_50'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
@@ -68,7 +71,8 @@ class RetinaNetModule(L.LightningModule):
         self.log('mar_small', map_dict['mar_small'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
         self.log('mar_medium', map_dict['mar_medium'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
         self.log('mar_large', map_dict['mar_large'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
-    
+        self.metric.reset()
+        
     def test_step(self, batch, batch_idx):
         images, targets = batch
         images = [img.float() for img in images]
@@ -76,6 +80,8 @@ class RetinaNetModule(L.LightningModule):
 
         preds = self.model(images, targets)
         self.metric.update(preds, targets)
+    
+    def test_epoch_end(self):
         map_dict = self.metric.compute()
         self.log('map', map_dict['map'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
         self.log('map_50', map_dict['map_50'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
@@ -89,4 +95,5 @@ class RetinaNetModule(L.LightningModule):
         self.log('mar_small', map_dict['mar_small'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
         self.log('mar_medium', map_dict['mar_medium'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
         self.log('mar_large', map_dict['mar_large'], on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=len(images))
-        
+
+        self.metric.reset()
